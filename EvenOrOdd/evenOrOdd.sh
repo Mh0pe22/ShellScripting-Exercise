02e9f46eb05700c9a41744174a4ebec9ac4745f8
Mh0pe22 <mohitbambharoliya2004@gmail.com>
2025-12-16 09:27:16 +0000

    Even or odd shell script added

---
+++ EvenOrOdd/evenOrOdd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<< EvenOrOdd
+Write a shell script that takes a number from the user and prints whether it is Even or Odd.
+EvenOrOdd
+
+read -p "Enter a number : " number
+
+if ((number % 2 == 0 ));
+then
+	echo "$number is even"
+else
+	echo "$number is odd"
+fi
